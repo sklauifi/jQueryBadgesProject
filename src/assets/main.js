435271e99e12a0d8632fb6e655a6952fdfc68195
sklauifi <sklauifi@hawaii.edu>
2017-03-11 16:57:10 -1000

    jquery4

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,44 +1,41 @@
 $(function() {
     
 $.ajax({
-    url: 'https://www.codeschool.com/users/sklauifi.json',
-    dataType: 'jsonp',
-    success: function(response) {
-    addCourses(response.courses.completed); 
+  url: 'https://www.codeschool.com/users/sklauifi.json',
+  dataType: 'jsonp',
+  success: function(response) {
+  addCourses(response.courses.completed); 
     }
   });
 
-    function addCourses(courses){
+function addCourses(courses){
      
     var $badges = $('#badges');
         
     courses.forEach(function(course) {
         
-    console.log(course);
+        console.log(course);
                     
-     var $course = $('div/>'),{
-        'class': 'course'
-    }).appendTo($badges);
+        var $course = $('div/>'),{
+            'class': 'course'
+        }).appendTo($badges);
         
-    $('<h3 />', {
-        text: course.title
-    }).appendTo($course); 
+        $('<h3 />', {
+            text: course.title
+        }).appendTo($course); 
         
         $('<img />',{
           src: course.badge
-          }).appendTo($course);
+        }).appendTo($course);
         
         $('<a />',{
-            'class': 'btn btn-primary',
+          'class': 'btn btn-primary',
             target: '_blank',
             href: course.url,
             text: 'See Course'
-        }).appendTo($course);
-        
-        
-    })               
+            }).appendTo($course);
+     })               
  }    
         
   });       
         
-});
