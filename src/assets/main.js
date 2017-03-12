47e8ac0a10742913e0cb12f1c8c641092404ff0f
sklauifi <sklauifi@hawaii.edu>
2017-03-11 22:30:39 -1000

    Badges final

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,41 +1,40 @@
-$(function () {
-    
+$(function() {
+
   $.ajax({
     url: 'https://www.codeschool.com/users/sklauifi.json',
     dataType: 'jsonp',
     success: function(response) {
-    addCourses(response.courses.completed); 
+     addCourses(response.courses.completed);
     }
   });
 
 function addCourses(courses){
-     
+   
     var $badges = $('#badges');
-        
-    courses.forEach(function(course) {
-        
+ 
+    courses.forEach(function(course){
+       
         console.log(course);
-                    
-        var $course = $('div />',{
+       
+        var $course = $('<div />',{
             'class': 'course'
         }).appendTo($badges);
-        
+       
         $('<h3 />', {
             text: course.title
-        }).appendTo($course); 
-        
-        $('<img />',{
-          src: course.badge
         }).appendTo($course);
-        
+       
+        $('<img />',{
+          src: course.badge 
+        }).appendTo($course);
+       
         $('<a />',{
-          'class': 'btn btn-primary',
+          'class':'btn btn primary',
           target: '_blank',
           href: course.url,
           text: 'See Course'
           }).appendTo($course);
-     })               
- }    
-        
- });       
-        
+    })
+}
+   
+});
